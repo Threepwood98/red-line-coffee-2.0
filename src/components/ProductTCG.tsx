ab1a6e6629647311821e0c963804ea2828f4eb6f
Threepwood98 <kadelgadoalonso@gmail.com>
2026-03-13 12:59:40 -0400

    chore: remove unused PokeScan1.0 component and adjust ProductTCG text size

--- src/components/ProductTCG.tsx
+++ src/components/ProductTCG.tsx
@@ -156,7 +156,7 @@ export default function ProductTCG({ product, className }: ProductTCGProps) {
             />
           </div>
           <div className="text-center uppercase">
-            <span className="block font-bebas-neue text-xl sm:text-2xl text-accent-foreground">
+            <span className="block font-bebas-neue text-lg sm:text-xl text-accent-foreground">
               {product.nameJP}
             </span>
             <span className="block font-rajdhani text-xs sm:text-sm text-accent-foreground italic">
